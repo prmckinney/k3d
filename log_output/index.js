b68a8b511c96924fb35295145c5cab201a8ae252
Patrick McKinney <git@prmckinney.com>
2026-07-31 15:21:31 -0500

    converting log_output to server with ingress

--- log_output/index.js
+++ log_output/index.js
@@ -1,13 +1,18 @@
 import { v4 as uuidv4 } from "uuid";
+import Koa from "koa";
 
 const uuid = uuidv4();
+const app = new Koa();
 
-const logUUID = () => {
+const PORT = process.env.PORT || 3000;
+
+app.use(async (ctx) => {
   const time = new Date();
 
+  if (ctx.path.includes("favicon.ico")) return;
   console.log(`${time}: ${uuid}`);
+  ctx.body = `${time}: ${uuid}`;
+});
 
-  setTimeout(logUUID, 5000);
-};
-
-logUUID();
+console.log(`Started with ${uuid}`);
+app.listen(PORT);
