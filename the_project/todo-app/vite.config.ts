327611aa9bb982ba3dc4fe5458c8ce61907f078b
Patrick McKinney <git@prmckinney.com>
2026-08-06 09:20:13 -0500

    Adding configuration to hardcoded values

--- the_project/todo-app/vite.config.ts
+++ the_project/todo-app/vite.config.ts
@@ -1,24 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  preview: {
-    port: 8080,
-    strictPort: true,
-  },
-  server: {
-    proxy: {
-      "^/todo|image": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    preview: {
+      port: parseInt(env.PORT) || 8080,
+      strictPort: true,
     },
-    port: 8080,
-    strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:8080",
-  },
+    server: {
+      proxy: {
+        "^/todo|image": {
+          target: env.BACKEND_URL || "http://localhost:3000",
+          changeOrigin: true,
+        },
+      },
+      port: parseInt(env.PORT) || 8080,
+      strictPort: true,
+      host: true,
+    },
+  };
 });
