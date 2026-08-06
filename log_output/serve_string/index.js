2d07a071b14b9d330a20976193a37c4ac108661e
Patrick McKinney <git@prmckinney.com>
2026-08-06 08:58:49 -0500

    Adding configmap to log

--- log_output/serve_string/index.js
+++ log_output/serve_string/index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 const directory = "/usr/src/app/files";
 const uuidFilePath = path.join(directory, "uuid.txt");
+const configPath = "/config/information.txt";
 
 const getFile = async (filePath) =>
   new Promise((res) => {
@@ -22,7 +23,12 @@ app.use(async (ctx) => {
   const uuid = await getFile(uuidFilePath);
   const response = await axios.get("http://ping-pong-svc:1235/pings");
   const pingpong = response.data;
-  ctx.body = `${uuid}\nPing \/ Pongs: ${pingpong}`;
+  const config = await getFile(configPath);
+  const message = process.env.MESSAGE;
+  ctx.body = `file content: ${config}`;
+  ctx.body += `env variable: MESSAGE=${message}`;
+  ctx.body += `${uuid}`;
+  ctx.body += `Ping \/ Pongs: ${pingpong}`;
 
   console.log(`${uuid}\nPing \/ Pongs: ${pingpong}`);
 });
