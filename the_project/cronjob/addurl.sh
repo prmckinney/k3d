4e099db1c98061f9ea1c536b6a8c84c33ee224d3
Patrick McKinney <git@prmckinney.com>
2026-08-07 21:34:09 -0500

    Updating addurl cronjob

--- the_project/cronjob/addurl.sh
+++ the_project/cronjob/addurl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 url=$(curl -I -s https://en.wikipedia.org/wiki/Special:Random | grep -i '^location:' | sed 's/location: /https:/')
 echo "Adding $url"
-$(psql postgresql://postgres:password@localhost:5432/postgres -c "INSERT INTO todos VALUES('Read $url');")
+$(psql postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} -c "INSERT INTO todos VALUES('Read $url');")
