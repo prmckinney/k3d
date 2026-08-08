d0163343dfeb424cdaf5a8a7dd39d84459d240cc
Patrick McKinney <git@prmckinney.com>
2026-08-07 22:13:10 -0500

    Fixing cronjob

--- the_project/cronjob/addurl.sh
+++ the_project/cronjob/addurl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 url=$(curl -I -s https://en.wikipedia.org/wiki/Special:Random | grep -i '^location:' | sed 's/location: /https:/' | tr -d '\r')
 echo "Adding $url"
-$(psql postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} -c "INSERT INTO todos VALUES('Read $url');")
+psql postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} -c "INSERT INTO todos VALUES('Read $url');"
